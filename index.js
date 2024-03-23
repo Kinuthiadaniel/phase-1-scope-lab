bd460914196737525ca40ea8d6f41f272a772580
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-03-23 14:37:54 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+
+return customerName.toUpperCase();
+}
+function setBestCustomer(){
+bestCustomer = "not bob"
+}
+function overwriteBestCustomer(){
+bestCustomer= "maybe bob"
+}
+const leastFavoriteCustomer = "alice"
+ function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "ben"
+  return leastFavoriteCustomer;
+ }
+upperCaseCustomerName();
+console.log(changeLeastFavoriteCustomer());
